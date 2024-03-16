3971df2544e79e20f7add66bf9f9fbe11223d4cd
x2Neptune <94178333+ZoiIntra@users.noreply.github.com>
2024-03-16 22:04:09 +0700

    Create __CheckGame.lua

---
+++ Luable/__CheckGame.lua
@@ -0,0 +1,20 @@
+_ = {}
+function _:Game()
+    __ = game.PlaceId
+    if __ == 2753915549 then
+        return "Blox Fruits"
+    elseif __ == 4442272183 then
+        return "Blox Fruits"
+    elseif __ == 7449423635 then
+        return "Blox Fruits"
+    elseif __ == 537413528 then
+        return "Build A Boat"
+    elseif __ == 4520749081 then
+        return "King Legacy"
+    elseif __ == 6381829480 then
+        return "King Legacy"
+    else
+        return "Blade Ball"
+    end
+end
+return _
